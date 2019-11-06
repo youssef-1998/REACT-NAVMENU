320dccf0da700c8a967ae24bfb2893e14ce749b5
youssef-1998 <bouzar26@gmail.com>
2019-11-06 13:08:51 +0100

    Updates

---
+++ precache-manifest.a620cf11a7ebffc647e4092eafb877a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417ab726938dc8bf8cd0b85103a99010",
+    "url": "/youssef-1998/REACT-NAVMENU/index.html"
+  },
+  {
+    "revision": "db15f4d9dfad727b35a5",
+    "url": "/youssef-1998/REACT-NAVMENU/static/css/main.1b247a36.chunk.css"
+  },
+  {
+    "revision": "ed3817da4bcc9f81138f",
+    "url": "/youssef-1998/REACT-NAVMENU/static/js/2.54c0e964.chunk.js"
+  },
+  {
+    "revision": "db15f4d9dfad727b35a5",
+    "url": "/youssef-1998/REACT-NAVMENU/static/js/main.4f9e1baa.chunk.js"
+  },
+  {
+    "revision": "5db6a447ca4bcc447588",
+    "url": "/youssef-1998/REACT-NAVMENU/static/js/runtime-main.dcfe306b.js"
+  }
+]);
